27d647d720bba7fbb1d9703103b8cb1bc3132977
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-05-17 18:37:21 -0400

    commit

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,1 +1,6 @@
 var earthquake_url = "hhttps://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonttps://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php"
+
+var myMap = L.map("mapid", {
+    center: [34.0754, -84.2941],
+    zoom: 2
+});
